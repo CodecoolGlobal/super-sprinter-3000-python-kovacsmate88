fec871b9d1837c5cf1979561b8fe0a162777032d
kovacsmate88 <kovacsmate321@gmail.com>
2022-06-29 21:26:26 +0200

    done

--- data_handler.py
+++ data_handler.py
@@ -7,4 +7,26 @@ STATUSES = ['planning', 'todo', 'in progress', 'review', 'done']
 
 
 def get_all_user_story():
-    return []
+    with open(DATA_FILE_PATH, newline='') as user_stories:
+        user_stories = list(csv.DictReader(user_stories))
+    return user_stories
+
+
+def add_user_story(user_story: dict):
+    with open(DATA_FILE_PATH, 'a', newline='') as user_stories:
+        writer = csv.DictWriter(user_stories, DATA_HEADER)
+        writer.writerow(user_story)
+
+
+def update_user_story(stories):
+    with open(DATA_FILE_PATH, 'w', newline='') as u_stories:
+        writer = csv.DictWriter(u_stories, DATA_HEADER)
+        writer.writeheader()
+        writer.writerows(stories)
+
+
+
+def generate_id():
+    user_stories = get_all_user_story()
+    id = max([int(user_story['id']) for user_story in user_stories]) + 1
+    return len(get_all_user_story()) + 1
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
+import data_handler
 
 app = Flask(__name__)
 
@@ -7,8 +9,54 @@ app = Flask(__name__)
 @app.route('/list')
 def route_list():
     user_stories = data_handler.get_all_user_story()
-
     return render_template('list.html', user_stories=user_stories)
+
+
+@app.route('/story', methods=['GET','POST'])
+def story():
+    if request.method == 'POST':
+        user_story = {}
+        user_story['id'] = data_handler.generate_id()
+        user_story['title'] = request.form['title']
+        user_story['user_story'] = request.form['story']
+        user_story['acceptance_criteria'] = request.form['acceptance-criteria']
+        user_story['business_value'] = request.form['value']
+        user_story['estimation'] = request.form['estimation']
+        user_story['status'] = 'planning'
+
+        data_handler.add_user_story(user_story)
+        return redirect('/')
+    return render_template('story.html')
+
+
+@app.route('/story/<int:id>', methods=['GET', 'POST'])
+def update_story(id):
+    if request.method == 'POST':
+        user_story = {}
+        user_story['id'] = id
+        user_story['title'] = request.form['title']
+        user_story['user_story'] = request.form['story']
+        user_story['acceptance_criteria'] = request.form['acceptance-criteria']
+        user_story['business_value'] = request.form['value']
+        user_story['estimation'] = request.form['estimation']
+        user_story['status'] = request.form['status']
+
+        user_stories = data_handler.get_all_user_story()
+
+        for row in user_stories:
+            if row['id'] == user_story['id']:
+                user_stories[user_stories.index(row)] = user_story
+
+        data_handler.update_user_story(user_stories)
+        return redirect('/')
+
+    user_stories = data_handler.get_all_user_story()
+    for row in user_stories:
+        if row['id'] == id:
+            user_story_to_be_updated = row
+    return render_template('update-story.html', user_story=user_story_to_be_updated, status_options=data_handler.STATUSES, id=id)
+
+#TODO ask about status: in progress - only showing in - when clicked to update it's not even selected
 
 if __name__ == '__main__':
     app.run(
